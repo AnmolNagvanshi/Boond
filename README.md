a292b7c27944e35e437be5674368104cdcae45cf
Anmol <anmolnagvanshi04@gmail.com>
2020-11-02 13:30:06 +0530

    removed django files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Boond
-A Blood Bank Management Portal developed in Django.
--- banks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
-
-# path("<int:bank_id>", views.bank, name="bank"),
-# path("<int:bank_id>/update", views.update, name="update"),
